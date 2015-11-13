8a460c9c3ce505b1edb915b8d2c3c661d778bd7e
Joshua Sachs <jawshasaurussex@gmail.com>
2015-11-13 16:41:13 -0700

    added functions

--- AudioPlayer/ViewController.swift
+++ AudioPlayer/ViewController.swift
@@ -8,27 +8,31 @@
 
 import UIKit
 import AVFoundation
+import MediaPlayer
+//import MediaPlayer
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MPMediaPickerControllerDelegate {
     
     var mp3Player: AVAudioPlayer = AVAudioPlayer()
+    var mediaPicker: MPMediaPickerController = MPMediaPickerController(mediaTypes: .AnyAudio)
     //var enableRate: Bool = Bool()
     //var rate: Float = 2.0
-   
+    
     
 
     @IBAction func play(sender: AnyObject) {
         playSong()
     }
+    
     @IBAction func pause(sender: AnyObject) {
-        if mp3Player.playing {
-            mp3Player.pause()
-        }
+       pauseSong()
     }
     
     @IBAction func faster(sender: AnyObject) {
         speedUp()
     }
+    
+       
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -41,8 +45,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //button functions
     func playSong() {
-        let mp3Path = NSBundle.mainBundle().pathForResource("Build Me", ofType: "mp3")
+        let mp3Path = NSBundle.mainBundle().pathForResource("vroom", ofType: "mp3")
         let song:NSURL = NSURL(fileURLWithPath: mp3Path!)
         
         
@@ -55,19 +60,46 @@ class ViewController: UIViewController {
     }
     
     func speedUp(){
-        let mp3Path = NSBundle.mainBundle().pathForResource("Build Me", ofType: "mp3")
+        let mp3Path = NSBundle.mainBundle().pathForResource("vroom", ofType: "mp3")
         let song:NSURL = NSURL(fileURLWithPath: mp3Path!)
         
         
         do {
             mp3Player = try AVAudioPlayer(contentsOfURL: song)
             mp3Player.enableRate = true
-            mp3Player.rate = 2.0
+            mp3Player.rate = 1.30
             mp3Player.play()
         } catch {
             print("Error, something went wrong")
             }
+    }
+    
+    func pauseSong() {
+        if mp3Player.playing {
+            mp3Player.pause()
+        } else {
+            mp3Player.play()
         }
+    }
+    
+    func mediaSelect(){
+        
+        if let picker = mediaPicker{
+            picker.delegate = self
+            picker.allowsPickingMultipleItems = true
+            picker.showsCloudItems = true
+            picker.prompt = "Pick a song please"
+            view.addSubview(picker.view)
+            
+            presentViewController(picker, animated: true, completion: nil)
+        } else {
+            print("Something didn't work")
+        }
+        
+    }
+    
+    
+    
     }
     
 
